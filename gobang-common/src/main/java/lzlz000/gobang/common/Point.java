6df565b2bcef35d03ce542c5666c5468e94ceb4b
liuzheng <lzlz0810@qq.com>
2019-11-09 02:44:55 +0800

    睡觉了

--- gobang-common/src/main/java/lzlz000/gobang/common/Point.java
+++ gobang-common/src/main/java/lzlz000/gobang/common/Point.java
@@ -8,4 +8,14 @@ import lombok.Getter;
 public class Point {
     private int x;
     private int y;
+    public int getIndex(int size){
+        return x + y * size;
+    }
+
+    public static Point pointOfIndex(int size, int index){
+        if (index >= size * size) {
+            throw new IllegalArgumentException();
+        }
+        return new Point(index%size,index/size);
+    }
 }
---
+++ gobang-robot/src/main/java/lzlz000/gobang/robot/move/MoveGenerator.java
@@ -0,0 +1,13 @@
+package lzlz000.gobang.robot.move;
+
+import lzlz000.gobang.common.Board;
+import lzlz000.gobang.common.PathNode;
+import lzlz000.gobang.common.Point;
+
+import java.util.List;
+
+public class MoveGenerator {
+    public List<PathNode> generate(Board board, Point point){
+        return null;
+    }
+}
