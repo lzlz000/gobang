dc4b088ce6d7f003a653ff1831e17d44685e3894
liuzheng <lzlz0810@qq.com>
2019-11-15 01:50:24 +0800

    测试机器人

--- gobang-robot/src/main/java/com/github/lzlz000/gobang/robot/robot0/MoveEvaluator.java
+++ gobang-robot/src/main/java/com/github/lzlz000/gobang/robot/robot0/MoveEvaluator.java
@@ -6,20 +6,16 @@ import com.github.lzlz000.gobang.common.game.Point;
 import java.util.function.Function;
 
 /**
- * 计算每一步的得分
+ * 计算每一步的得分//
  */
 class MoveEvaluator {
 
     private final LineEvaluation lineEvaluation = new LineEvaluation();
 
-<<<<<<< HEAD:gobang-robot/src/main/java/com/github/lzlz000/gobang/robot/evalution/MoveEvaluatorImpl.java
-    public int evaluate(Point p, Board board){
-=======
     /**
      * @return evalution里的score一个非负数 如果是对方的棋子请减掉
      */
     Evaluation evaluate(Board board,Point p){
->>>>>>> 163ebdc789438babcf6ca23344c65ceb12ce871d:gobang-robot/src/main/java/com/github/lzlz000/gobang/robot/robot0/MoveEvaluator.java
         Line[] lines = {direction1(p, board),direction2(p, board),direction3(p, board), direction4(p, board)};
         int score = 0;
         for (Line line : lines) {
@@ -42,8 +38,8 @@ class MoveEvaluator {
     private Line direction1(Point p, Board board){
         Line line = new Line();
         line.addMine();
-        spliceLine(p, board, line, true,  p0 -> new Point(p.getX()-1 ,p.getY()));
-        spliceLine(p, board, line, false,  p0 -> new Point(p.getX()+1 ,p.getY()));
+        spliceLine(p, board, line, true,  p0 -> new Point(p0.getX()-1 ,p0.getY()));
+        spliceLine(p, board, line, false,  p0 -> new Point(p0.getX()+1 ,p0.getY()));
         return line;
     }
 
@@ -51,9 +47,9 @@ class MoveEvaluator {
     private Line direction2(Point p, Board board){
         Line line = new Line();
         line.addMine();
-        spliceLine(p, board, line, true,  p0 -> new Point(p.getX()-1 ,p.getY()-1));
+        spliceLine(p, board, line, true,  p0 -> new Point(p0.getX()-1 ,p0.getY()-1));
         // TODO 死循环
-        spliceLine(p, board, line, false,  p0 -> new Point(p.getX()+1 ,p.getY()+1));
+        spliceLine(p, board, line, false,  p0 -> new Point(p0.getX()+1 ,p0.getY()+1));
         return line;
     }
 
@@ -61,8 +57,8 @@ class MoveEvaluator {
     private Line direction3(Point p, Board board){
         Line line = new Line();
         line.addMine();
-        spliceLine(p, board, line, true,  p0 -> new Point(p.getX() ,p.getY()-1));
-        spliceLine(p, board, line, false,  p0 -> new Point(p.getX() ,p.getY()+1));
+        spliceLine(p, board, line, true,  p0 -> new Point(p0.getX() ,p0.getY()-1));
+        spliceLine(p, board, line, false,  p0 -> new Point(p0.getX() ,p0.getY()+1));
         return line;
     }
 
@@ -70,8 +66,8 @@ class MoveEvaluator {
     private Line direction4(Point p, Board board){
         Line line = new Line();
         line.addMine();
-        spliceLine(p, board, line, true,  p0 -> new Point(p.getX()+1 ,p.getY()-1));
-        spliceLine(p, board, line, false,  p0 -> new Point(p.getX()-1 ,p.getY()+1));
+        spliceLine(p, board, line, true,  p0 -> new Point(p0.getX()+1 ,p0.getY()-1));
+        spliceLine(p, board, line, false,  p0 -> new Point(p0.getX()-1 ,p0.getY()+1));
         return line;
     }
 
